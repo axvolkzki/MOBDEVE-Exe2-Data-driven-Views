220e23624a6cd044e933a30bbb13fb7a2720a7bb
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-10-21 21:26:48 +0800

    Implemented PagerSnapHelper

--- app/src/main/java/com/mobdeve/s14/abenoja/ameliajoyce/exercise2/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s14/abenoja/ameliajoyce/exercise2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mobdeve.s14.abenoja.ameliajoyce.exercise2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.SnapHelper
 import com.mobdeve.s14.abenoja.ameliajoyce.exercise2.databinding.ActivityMainBinding
