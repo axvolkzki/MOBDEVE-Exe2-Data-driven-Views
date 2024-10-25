bbabf24fb74ce9cb0dda3e440fa2fad084fda3cb
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-10-25 22:17:18 +0800

    Finished exercise 2

--- app/src/main/java/com/mobdeve/s14/abenoja/ameliajoyce/exercise2/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s14/abenoja/ameliajoyce/exercise2/MainActivity.kt
@@ -3,9 +3,6 @@ package com.mobdeve.s14.abenoja.ameliajoyce.exercise2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
-import androidx.recyclerview.widget.PagerSnapHelper
-import androidx.recyclerview.widget.SnapHelper
 import com.mobdeve.s14.abenoja.ameliajoyce.exercise2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +20,6 @@ class MainActivity : AppCompatActivity() {
         // Anime List
         viewBinding.rcvAnimeList.adapter = AnimeAdapter(DataGenerator.generateAnimeTitleData())
         viewBinding.rcvAnimeList.layoutManager = linearLayoutHorizontal
-
-        // Snap Helper
-        val snapHelper: SnapHelper = PagerSnapHelper()
-        snapHelper.attachToRecyclerView(viewBinding.rcvAnimeList)
-
 
         // Episode List
         viewBinding.rcvEpisodeList.adapter = EpisodeAdapter(DataGenerator.generateRecentReviewsData())
